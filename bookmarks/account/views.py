e60e5a3621f430c4d6cc38584d5e8e440f18c1cd
Bartosz Cholewa <bbcholewa@gmail.com>
2022-04-03 15:32:38 +0200

    chapter 4 - messages framework

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -66,6 +67,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Zmiany zostały zapisane")
+        else:
+            messages.error(request, "Błąd - zmiany nie zostały zapisane")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
